3b64c616b43b8ab39d6e18ff4b664c60247778ab
jdwalkerzhere <jdwalkerzhere@comcast.net>
2025-03-05 09:54:59 -0500

    basic file server handling

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"net/http"
 )
 
+func healthz(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
-	serveMux := http.ServeMux{}
+	serveMux := http.NewServeMux()
 	server := http.Server{
-		Handler: &serveMux,
+		Handler: serveMux,
 		Addr:    ":8080",
 	}
+	prefixHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", prefixHandler)
+	serveMux.HandleFunc("/healthz", healthz)
 	server.ListenAndServe()
 }
