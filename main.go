ce824abc43c44d410f691b6f330659d2193cbd2a
jdwalkerzhere <jdwalkerzhere@comcast.net>
2025-03-07 12:08:01 -0500

    added Validate Chirp

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -35,6 +36,37 @@ func (c *apiConfig) reset(w http.ResponseWriter, _ *http.Request) {
 	c.fileServerHits.Swap(0)
 }
 
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type Chirp struct {
+		Body string `json:"body"`
+	}
+	type httpError struct {
+		Message string `json:"error"`
+	}
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	encoder := json.NewEncoder(w)
+
+	chirp := Chirp{}
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		w.WriteHeader(500)
+		encoder.Encode(httpError{"Something went wrong"})
+		return
+	}
+
+	if len(chirp.Body) > 140 {
+		w.WriteHeader(400)
+		encoder.Encode(httpError{"Chirp is too long"})
+		return
+	}
+
+	success := map[string]bool{"valid": true}
+	w.WriteHeader(200)
+	encoder.Encode(success)
+}
+
 func main() {
 	serveMux := http.NewServeMux()
 	server := http.Server{
@@ -47,5 +79,6 @@ func main() {
 	serveMux.HandleFunc("GET /api/healthz", healthz)
 	serveMux.HandleFunc("GET /admin/metrics", metrics.metrics)
 	serveMux.HandleFunc("POST /admin/reset", metrics.reset)
+	serveMux.HandleFunc("POST /api/validate_chirp", validateChirp)
 	server.ListenAndServe()
 }
